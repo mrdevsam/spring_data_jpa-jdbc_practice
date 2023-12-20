fd319b5cca8564eea5b8aba88b372bd579d972df
mrdevsam <mdshoykotislam05@gmail.com>
2023-12-20 18:48:53 +0000

    initial commit

---
+++ src/main/java/com/example/springdatajpa_jdbc/SpringDataJpaJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdatajpa_jdbc/SpringDataJpaJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
