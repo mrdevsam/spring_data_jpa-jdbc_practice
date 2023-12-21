90f2b0630b7cd8fb254da7c1da9c5c4b9af317cd
mrdevsam <mdshoykotislam05@gmail.com>
2023-12-21 16:34:57 +0000

    adding logic on getById

--- src/main/java/com/example/springdatajpa_jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/com/example/springdatajpa_jdbc/dao/AuthorDaoImpl.java
@@ -2,12 +2,42 @@ package com.example.springdatajpa_jdbc.dao;
 
 import com.example.springdatajpa_jdbc.domain.Author;
 import org.springframework.stereotype.Component;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
+	private final DataSource ds;
+
+	public AuthorDaoImpl(DataSource ds) {
+		this.ds = ds;
+	}
+
 	@Override
 	public Author getById(Long id) {
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			cn = ds.getConnection();
+			st = cn.createStatement();
+			rs = st.executeQuery("select * from author where id = " + id);
+
+			if(rs.next()) {
+				Author author = new Author();
+				author.setId(id);
+				author.setFirstName(rs.getString("first_name"));
+				author.setLastName(rs.getString("last_name"));
+				return author;
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
