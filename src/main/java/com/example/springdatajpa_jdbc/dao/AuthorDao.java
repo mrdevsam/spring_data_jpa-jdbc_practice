9e4712b9ff28b53b66b679a90019da800a7858e1
mrdevsam <mdshoykotislam05@gmail.com>
2023-12-22 17:16:45 +0000

    adding findAuthorByName in dao

--- src/main/java/com/example/springdatajpa_jdbc/dao/AuthorDao.java
+++ src/main/java/com/example/springdatajpa_jdbc/dao/AuthorDao.java
@@ -4,4 +4,5 @@ import com.example.springdatajpa_jdbc.domain.Author;
 
 public interface AuthorDao {
 	Author getById(Long id);
+	Author findAuthorByName(String firstName, String lastName);
 }
--- src/main/java/com/example/springdatajpa_jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/com/example/springdatajpa_jdbc/dao/AuthorDaoImpl.java
@@ -55,4 +55,48 @@ public class AuthorDaoImpl implements AuthorDao {
 		
 		return null;
 	}
+
+	@Override
+	public Author findAuthorByName(String firstName, String lastName) {
+
+		Connection cn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+			
+		try {
+			cn = ds.getConnection();
+			pst = cn.prepareStatement("select * from author where first_name = ? and last_name = ?");
+			pst.setString(1, firstName);
+			pst.setString(2, lastName);
+			rs = pst.executeQuery();
+
+			if(rs.next()) {
+				Author author = new Author();
+				author.setId(rs.getLong("id"));
+				author.setFirstName(rs.getString("first_name"));
+				author.setLastName(rs.getString("last_name"));
+				return author;
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pst != null) {
+					pst.close();
+				}
+				
+				if(cn != null) {
+					cn.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+			
+		return null;
+	}
 }
