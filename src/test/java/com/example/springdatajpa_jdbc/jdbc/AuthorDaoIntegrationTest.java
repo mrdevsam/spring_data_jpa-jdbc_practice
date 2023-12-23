3596de2cb9139e3f5212bd0034ae64640596943c
mrdevsam <mdshoykotislam05@gmail.com>
2023-12-23 15:52:42 +0000

    adding dao implementations for Book

--- src/test/java/com/example/springdatajpa_jdbc/jdbc/AuthorDaoIntegrationTest.java
+++ src/test/java/com/example/springdatajpa_jdbc/jdbc/AuthorDaoIntegrationTest.java
@@ -19,6 +19,64 @@ public class AuthorDaoIntegrationTest {
 	@Autowired
 	AuthorDao authorDao;
 
+	@Autowired
+	BookDao bookDao;
+
+	@Test
+	void testDeleteBook() {
+		Book book = new Book();
+		book.setIsbn("1234");
+		book.setPublisher("Self");
+		book.setTitle("My Book");
+		book.setAuthorId(3L);
+		Book saved = bookDao.saveNewBook(book);
+
+		bookDao.deleteBookById(saved.getId());
+
+		Book deleted = bookDao.getById(saved.getId());
+		assertThat(deleted).isNull();
+	}
+
+	@Test
+	void testUpdateBook() {
+		Book book = new Book();
+		book.setIsbn("1234");
+		book.setPublisher("Self");
+		book.setTitle("My Book");
+		book.setAuthorId(3L);
+		Book saved = bookDao.saveNewBook(book);
+
+		saved.setTitle("New Book");
+		bookDao.updateBook(saved);
+
+		Book fetched = bookDao.getById(saved.getId());
+		assertThat(fetched.getTitle()).isEqualTo("New Book");
+	}
+
+	@Test
+	void testSaveBook() {
+		Book book = new Book();
+		book.setIsbn("1234");
+		book.setPublisher("Self");
+		book.setTitle("My Book");
+		book.setAuthorId(3L);
+		Book saved = bookDao.saveNewBook(book);
+
+		assertThat(saved).isNotNull();
+	}
+
+	@Test
+	void testGetBookByTitle() {
+		Book book = bookDao.findBookByTitle("Clean Code");
+		assertThat(book).isNotNull();
+	}
+
+	@Test
+	void testGetBookById() {
+		Book book = bookDao.getById(3L);
+		assertThat(book.getId()).isNotNull();
+	}
+
 	@Test
 	void testGetAuthor() {
 		Author author = authorDao.getById(1L);
